ea2c09638ea7441c9e56c9d5ed05e6e0db7c38ff
Jonathan Kula <kulboy121@gmail.com>
2018-02-17 20:37:22 -0800

    Just testing out stdlib for now...

---
+++ jdkula/chat-connector/functions/__main__.js
@@ -0,0 +1,9 @@
+const lib = require('lib');
+
+/**
+ * Returns general service status information
+ * @returns {string}
+ */
+module.exports = async (context) => {
+    return "Up and ready to go"
+};
