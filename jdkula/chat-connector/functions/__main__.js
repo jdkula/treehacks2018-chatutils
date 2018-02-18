4df671f802a7b2dc8dd642b662b2aa9383823163
Shyue <danielchenrules@gmail.com>
2018-02-18 01:42:29 -0500

    Starting working on stuff.

--- jdkula/chat-connector/functions/__main__.js
+++
@@ -1,9 +0,0 @@
-const lib = require('lib');
-
-/**
- * Returns general service status information
- * @returns {string}
- */
-module.exports = async (context) => {
-    return "Up and ready to go"
-};
---
+++ jdkula/chat-util/functions/__main__.js
@@ -0,0 +1,11 @@
+const lib = require('lib');
+
+/**
+* A basic I'M DYING function
+* @returns {string}
+*/
+module.exports = async (context) => {
+
+  return "If this doesn't work I don't even know."
+
+};
