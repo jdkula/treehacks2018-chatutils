2f5693c241fcac86a523b0349130a115cd86d557
Jonathan Kula <kulboy121@gmail.com>
2018-02-18 01:20:28 -0800

    Added convert, resize, resize_percent, and am working on svg_to_raster

--- jdkula/chat-util/functions/image/resize_percent.js
+++ jdkula/chat-util/functions/image/resize_percent.js
@@ -5,32 +5,14 @@ const jimp = require('jimp');
 /**
  * Gives basic image status info.
  * @param {string} image A base64-encoded image to process
- * @param {integer} x1 The x-coord of the top-left corner of the crop
- * @param {integer} y1 The y-coord of the top-left corner of the crop
- * @param {integer} x2 The x-coord of the bottom-right corner of the crop
- * @param {integer} y2 The y-coord of the bottom-right corner of hte crop
- * @param {string} shape One of: "square", "oval"
+ * @param {integer} percent The percent to resize to
  * @returns {string} A base64-encoded resultant image.
  */
-module.exports = async (image, x1, y1, x2, y2, shape = "square", context) => {
+module.exports = async (image, percent, context) => {
 
     let imageBuffer = Buffer.from(image, 'base64');
     let imageProcessor = await jimp.read(imageBuffer);
-    imageProcessor.crop(x1, y1, x2 - x1, y2 - y1);
-    if(shape === "oval") {
-        const width = imageProcessor.bitmap.width;
-        const height = imageProcessor.bitmap.height;
-        imageProcessor.scan(0, 0, width, height, (x, y, index) => {
-            if(isInCircle(x, y, width, height)) {
-                if(imageProcessor.bitmap !== undefined) {
-                    imageProcessor.bitmap.data[index] = 0; // Red
-                    imageProcessor.bitmap.data[index + 1] = 0; // Green
-                    imageProcessor.bitmap.data[index + 2] = 0; // Blue
-                    imageProcessor.bitmap.data[index + 3] = 255; // Alpha
-                }
-            }
-        });
-    }
+    imageProcessor.resize(imageProcessor.bitmap.width * percent, imageProcessor.bitmap.height * percent);
     return await getBase64(imageProcessor);
 
 };
